4ba29a72ce5255af2e29907a111e93a9e4ef8ec3
Vovnenko Oleksii <kespa71for@gmail.com>
2022-09-07 11:30:34 +0300

    create component First Screen

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from 'react';
 import Header from "./components/header/Header";
+import FirstScreen from "./components/FirstScreen";
+import Users from "./components/users/Users";
 
 const Layout: FC = () => {
     return (
         <>
             <Header/>
+            <FirstScreen/>
+            <Users/>
         </>
     );
 };
--- src/components/FirstScreen.tsx
+++ src/components/FirstScreen.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {FC} from 'react';
+import BaseBtn from "./UI/button/BaseBtn";
 
-const FirstScreen = () => {
+const FirstScreen :FC= () => {
     return (
-        <div>
-            
+        <div className='first-screen'>
+
+            <div className="block-description">
+                <h1 className="heading-screen">Test assignment for front-end developer</h1>
+                <span className='description'>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</span>
+            </div>
+
+            <BaseBtn children={'Sign up'}/>
         </div>
     );
 };
 
-export default FirstScreen;
+export default React.memo(FirstScreen);
